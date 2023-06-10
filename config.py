9f603bcd1e9a1b434597e5bb6e9c0281930b95fb
Alex <alexander-replyansky@yandex.ru>
2023-06-10 17:56:10 +0300

    updating

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ class Config:
     app.config['SECRET_KEY'] = ''.join(choice(printable) for i in range(50))
     app.config['DEBUG'] = True
     app.config[
-        'SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Microlab2gavno9@localhost/app_db"
+        'SQLALCHEMY_DATABASE_URI'] = "postgresql://alegofrenicht:jIdI1TF6x5h9YR9x0wIjo7FNbn9R5Uah@dpg-ci28kiu7avj2t331kri0-a.frankfurt-postgres.render.com/app_database_mce7"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
