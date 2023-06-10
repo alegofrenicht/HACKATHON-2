9e6cb8694c2c4cfbba7cd8657d95976ac9a6542b
Alex <alexander-replyansky@yandex.ru>
2023-06-10 18:09:32 +0300

    updating

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 from app import app
 from random import choice
 from string import printable
+from os import getenv
 
 
 class Config:
     app.config['SECRET_KEY'] = ''.join(choice(printable) for i in range(50))
     app.config['DEBUG'] = True
+    SQLALCHEMY_DATABASE_URI = getenv('DB_URI')
     app.config[
-        'SQLALCHEMY_DATABASE_URI'] = "postgresql://alegofrenicht:jIdI1TF6x5h9YR9x0wIjo7FNbn9R5Uah@dpg-ci28kiu7avj2t331kri0-a.frankfurt-postgres.render.com/app_database_mce7"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+        'SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
